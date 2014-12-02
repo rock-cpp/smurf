92dd926baa1e6e848d32e0da02bf5a1a47293b0f
Janosch Machowinski <janosch.machowinski@dfki.de>
2014-12-02 10:34:48 +0100

    added dummy implementations of methods

--- src/Smurf.cpp
+++ src/Smurf.cpp
@@ -1,4 +1,19 @@
 #include "Smurf.hpp"
 
+smurf::Robot::Robot()
+{
+
+}
+
+void smurf::Robot::loadFromSmurf(const std::string& path)
+{
+
+}
+
+smurf::Transformation::Transformation(smurf::Frame* sourceFrame, smurf::Frame* targetFrame) : name(sourceFrame->getName() + "2" + targetFrame->getName()), sourceFrame(sourceFrame), targetFrame(targetFrame)
+{
+
+}
+
 
 
