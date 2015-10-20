6417cbb338112f80d39a92c1942980afdbb1c7fe
basadi <behnam.asadi@dfki.de>
2015-10-20 17:17:56 +0200

    get method for accessing smurf sensors added. also the constructor was empty and it was not doing anything

--- src/Smurf.hpp
+++ src/Smurf.hpp
@@ -53,6 +53,11 @@ class Sensor
 {
 public:
     Sensor(const std::string &name, const std::string &type, const std::string &taskInstanceName, Frame *inFrame);
+    Sensor();
+    std::string getname();
+    std::string gettype();
+    std::string gettaskInstanceName();
+    Frame * getattachmentPoint();
 private:
     std::string name;
     std::string type;
