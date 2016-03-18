07ec61bb67e98e326a5b75fed95423e25b27e5d2
Raul Dominguez <raul.dominguez@dfki.de>
2016-03-18 17:42:50 +0100

    The motorMap has to have a 1 in the axis

--- src/Motor.cpp
+++ src/Motor.cpp
@@ -3,5 +3,6 @@
 smurf::Motor::Motor(configmaps::ConfigMap map)
 {
     motorMap = map;
+    motorMap["axis"] = 1; // TODO Why? This value is hardcoded in the current version of smurf (see SMURF::AddConfigMap)
     name = static_cast<std::string>(map["name"]);
 }
