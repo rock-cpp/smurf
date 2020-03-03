0f10b4af217f39dd379e027c5ee7d9f254e7c6bc
Anna Born <anna.born@dfki.de>
2020-03-03 18:32:26 +0100

    feature: load smurf without annotation (task,port linking for transformation)

--- src/DynamicTransformation.cpp
+++ src/DynamicTransformation.cpp
@@ -1,12 +1,18 @@
 #include "DynamicTransformation.hpp"
 
 
-smurf::DynamicTransformation::DynamicTransformation(const std::string& name, smurf::Frame* sourceFrame, smurf::Frame* targetFrame, const std::string& provider, const std::string& port): 
+smurf::DynamicTransformation::DynamicTransformation(const std::string& name, smurf::Frame* sourceFrame, smurf::Frame* targetFrame, const std::string& provider, const std::string& port):
     Transformation(name, sourceFrame, targetFrame), providerName(provider), providerPortName(port)
 {
 }
 
-smurf::DynamicTransformation::DynamicTransformation(smurf::Frame* sourceFrame, smurf::Frame* targetFrame, const std::string& provider, const std::string& port): 
+smurf::DynamicTransformation::DynamicTransformation(smurf::Frame* sourceFrame, smurf::Frame* targetFrame, const std::string& provider, const std::string& port):
     Transformation(sourceFrame, targetFrame), providerName(provider), providerPortName(port)
 {
+}
+
+smurf::DynamicTransformation::DynamicTransformation(const std::string &name, Frame *sourceFrame, Frame *targetFrame):
+    Transformation(sourceFrame, targetFrame), providerName(""), providerPortName("")
+{
+
 }
