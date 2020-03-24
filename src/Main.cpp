024d99f169ec5be8354fd80f73b47dd285d4060f
Anna <anna.born@dfki.de>
2020-03-24 19:20:17 +0100

    feature: add user-defined path in smurf_bin

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,8 +2,12 @@
 #include "Robot.hpp"
 int main(int argc, char** argv)
 {
+    if (argc != 2) 
+        std::runtime_error("call executable with one agrument: full or relative path to the smurf file");
+    
+    std::string smurf_path = argv[1];
     smurf::Robot robot;
-    robot.loadFromSmurf("../spaceclimber/SpaceClimber.smurf");
+    robot.loadFromSmurf(smurf_path);
     
 	return 0;
 }
