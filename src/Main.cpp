9d26160f914d53672eea8bf0db7ab6e800467027
Anna <anna.born@dfki.de>
2020-04-02 12:26:41 +0200

    clean

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "Robot.hpp"
-
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    if (argc != 2) 
         std::runtime_error("call executable with one agrument: full or relative path to the smurf file");
+    
     std::string smurf_path = argv[1];
     smurf::Robot robot;
     robot.loadFromSmurf(smurf_path);
-
+    
 	return 0;
 }
