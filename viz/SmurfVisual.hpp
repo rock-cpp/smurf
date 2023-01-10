f71ec829e0bcb0b9afad04b3ccc82588e0827055
Anna Born <anna.born@dfki.de>
2023-01-10 11:27:32 +0100

    add material to the mesh

--- viz/SmurfVisual.hpp
+++ viz/SmurfVisual.hpp
@@ -4,7 +4,8 @@
 #include <osg/Shape>
 #include <osg/ShapeDrawable>
 #include <osg/Geode>
-#include <osgDB/ReadFile> 
+#include <osg/Material>
+#include <osgDB/ReadFile>
 #include <urdf_model/model.h>
 
 namespace vizkit3d
@@ -38,7 +39,29 @@ namespace vizkit3d
                 urdf::MeshSharedPtr mesh = urdf::dynamic_pointer_cast<urdf::Mesh>(visual->geometry);
                 assert(mesh.get() != nullptr);
                 std::cout << "MESH: " << mesh->filename << std::endl;
-                osg::Node* meshNode = osgDB::readNodeFile(mesh->filename); 
+                osg::Node* meshNode = osgDB::readNodeFile(mesh->filename);
+
+                // TODO: move to SmurfVisual constructor, since this is generall functionality for all visuals
+                // TODO: add texture to the meshNode
+
+                // set Material
+                smurf::Material material = visual->getMaterial();
+                osg::Vec4 ambientColor = cvtOSGColor(material.getAmbientColor());
+                osg::Vec4 specularColor = cvtOSGColor(material.getSpecularColor());
+                osg::Vec4 diffuseColor = cvtOSGColor(material.getDiffuseColor());
+                float shininess = material.getShininess();
+
+                // TODO: taken from mars/graphics/osg_material_manager/OsgMaterial
+                osg::ref_ptr<osg::Material> osgMaterial = new osg::Material();;
+                osgMaterial->setColorMode(osg::Material::OFF);
+                osgMaterial->setAmbient(osg::Material::FRONT_AND_BACK, ambientColor);
+                osgMaterial->setSpecular(osg::Material::FRONT_AND_BACK, specularColor);
+                osgMaterial->setDiffuse(osg::Material::FRONT_AND_BACK, diffuseColor);
+                //material->setEmission(osg::Material::FRONT_AND_BACK, getColor("emissionColor"));
+                osgMaterial->setShininess(osg::Material::FRONT_AND_BACK, shininess);
+                //material->setTransparency(osg::Material::FRONT_AND_BACK, map.get("transparency", 0.0));
+
+                meshNode->getOrCreateStateSet()->setAttributeAndModes(osgMaterial.get(), osg::StateAttribute::ON);
                 addChild(meshNode);
             }
             void addBox(const std::shared_ptr<smurf::Visual> visual)
@@ -75,6 +98,15 @@ namespace vizkit3d
                 osg::Geode* geode = new osg::Geode();
                 geode->addDrawable(sphereDrawable);
                 addChild(geode);
+            }
+
+            osg::Vec4 cvtOSGColor(urdf::Color color) {
+                osg::Vec4 c(0, 0, 0, 1);
+                c[0] = color.r;
+                c[1] = color.g;
+                c[2] = color.b;
+                c[3] = color.a;
+                return c;
             }
     };
 }
